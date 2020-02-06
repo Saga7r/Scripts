7b9389d6f65a1388ed859d142128080b21f80858
Saga7r <41799562+Saga7r@users.noreply.github.com>
2020-02-06 21:54:49 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ apt-get  update
+ apt-get  upgrade
+ 
+
+ apt-get install -y libcurl4-openssl-dev
+ apt-get install -y libssl-dev
+ apt-get install -y jq
+ apt-get install -y ruby-full
+ apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev
+ apt-get install -y build-essential libssl-dev libffi-dev python-dev
+ apt-get install -y python-setuptools
+ apt-get install -y libldns-dev
+ apt-get install -y python3-pip
+ apt-get install -y python-pip
+ apt-get install -y python-dnspython
+ apt-get install -y git
+ apt-get install -y rename
+
